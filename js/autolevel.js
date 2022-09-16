4be85bae6b02345ea2bc2301200b3d01480d6147
zuils <vijaylemon05@gmail.com>
2022-09-15 21:18:43 -0400

    updated calc for unity saves

--- js/autolevel.js
+++ js/autolevel.js
@@ -1,4 +1,7 @@
 function generateNewSave(data, outsiders) {
+    let txt = $("#savegame").val();
+    let zlib = "7a990d405d2c6fb93aa8fbb0ec1a3b23";
+	let deflate = "7e8bb5a89f2842ac4af01b3b7e228592";
     let a = [1, 2, 3, 5, 6, 7, 8, 9, 10];
     for (let i=0; i<9; i++) {
         let outsiderID = a[i];
@@ -12,8 +15,9 @@ function generateNewSave(data, outsiders) {
     }
     data.ancientSouls = outsiders[9];
     let dataString = JSON.stringify(data);
-    let encodedData = pako.deflate(dataString, { to: 'string' });
-    $("#outputsave").val("7a990d405d2c6fb93aa8fbb0ec1a3b23" + btoa(encodedData));
+    encodedData = txt.substring(0, 32) == zlib ? pako.deflate(dataString, { to: 'string' }) : pako.deflateRaw(dataString, { to: 'string' });
+    $("#outputsave").val(txt.substring(0, 32) == zlib ? zlib + btoa(encodedData) : deflate + btoa(encodedData));
+    
 }
 
 function clearOutput() {
--- js/readSave.js
+++ js/readSave.js
@@ -1,16 +1,26 @@
 function readSave() {
 	let txt = $("#savegame").val();
 	let data;
-	if (txt.indexOf("Fe12NAfA3R6z4k0z") > -1 || txt.substring(0, 32) == "7a990d405d2c6fb93aa8fbb0ec1a3b23") {
-		if (txt.substring(0, 32) == "7a990d405d2c6fb93aa8fbb0ec1a3b23") {
+	let ANTI_CHEAT_CODE = "Fe12NAfA3R6z4k0z";
+	let zlib = "7a990d405d2c6fb93aa8fbb0ec1a3b23";
+	let deflate = "7e8bb5a89f2842ac4af01b3b7e228592";
+	if (txt.indexOf(ANTI_CHEAT_CODE) > -1 || txt.substring(0, 32) == zlib || txt.substring(0, 32) == deflate) {
+		if (txt.substring(0, 32) == zlib) {
 			try {
 				data = JSON.parse(pako.inflate(atob(txt.substring(32)), {to: 'string'}));
 			} catch(e) {
 				$("#savegame").val("");
 				return;
 			}
+		} else if (txt.substring(0, 32) == deflate) {
+			try {
+				data = JSON.parse(pako.inflateRaw(atob(txt.substring(32)), {to: 'string'}));
+			} catch(e) {
+				$("#savegame").val("");
+				return;
+			}
 		} else {
-			var result = txt.split("Fe12NAfA3R6z4k0z");
+			var result = txt.split(ANTI_CHEAT_CODE);
 			txt = "";
 			for (var i = 0; i < result[0].length; i += 2) {
 				txt += result[0][i];
